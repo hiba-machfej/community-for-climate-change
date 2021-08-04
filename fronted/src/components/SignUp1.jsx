626fc659ef3aba1fe50c8f2db4e120788696b8d6
hiba <hiba.mashfij@gmail.com>
2021-08-04 20:35:00 +0300

    Added sign-in link to sign up

--- fronted/src/components/SignUp1.jsx
+++ fronted/src/components/SignUp1.jsx
@@ -1,4 +1,5 @@
 import logo from '../images/logo.png';
+import { Link } from 'react-router-dom';
 
 
 const Example = () => {
@@ -73,7 +74,24 @@ const Example = () => {
               </button>
             </div>
           </form>
+          <div className="relative my-10">
+              <div className="absolute inset-0 flex items-center">
+                <div className="w-full border-t border-gray-300" />
+              </div>
+              <div className="relative flex justify-center text-sm">
+                <span className="px-2 bg-white text-gray-500">Or If you already have an Account</span>
+              </div>
+            </div>
+            <Link to="sign-in">
+            <button
+                type="submit"
+                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-400 hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                Sign In
+              </button>
+              </ Link>
         </div>
+        
       </div>
     </div>
   )
