5752239ce2a20762b9032005c07ddef724f4d9bb
Ziad Baroudi <ziad_baroudi@hotmail.com>
2021-08-08 14:13:42 +0300

    merging backend

--- site/models/user.js
+++ site/models/user.js
@@ -1,84 +1,26 @@
-// const fs = require("fs");
-// const path = require("path");
-// const db = require("../util/database");
-
-// const filePath = path.join(
-//   path.dirname(process.mainModule.filename),
-//   "data",
-//   "users.json"
-// );
-
-// const getUsersFromFiles = (callBack) => {
-//   fs.readFile(filePath, (err, fileContenet) => {
-//     if (err) {
-//       callBack([]);
-//     } else {
-//       callBack(JSON.parse(fileContenet));
-//     }
-//   });
-// };
-
-// module.exports = class User {
-//   constructor(id, firstName, lastName, email, password) {
-//     this.id = id;
-//     this.firstName = firstName;
-//     this.lastName = lastName;
-//     this.email = email;
-//     this.password = password;
-//   }
-//   save() {
-//     return db.execute(
-//       "INSERT INTO users (firstName, lastName, email, password) VALUES (?, ?, ?, ?)",
-//       [this.firstName, this.lastName, this.email, this.password]
-//     );
-//   }
-//   static fetchUsers() {
-//     return db.execute("SELECT * FROM USERS");
-//   }
-
-//   static singIn(email, password, callBack) {
-//     getUsersFromFiles((users) => {
-//       const user = users.find(
-//         (user) => user.email === email && user.password === password
-//       );
-//       callBack(user);
-//     });
-//   }
-// };
-
 const { Sequelize, DataTypes } = require("sequelize");
 const db = require("../util/database");
 const bcrypt = require("bcrypt");
 
-const User = db.define(
-  "User",
-  {
-    // Model attributes are defined here
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    firstName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    lastName: {
-      type: DataTypes.STRING,
-    },
+const User = db.define("User", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
   },
-  {
-    freezeTableName: true,
-  }
-);
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
 
 User.beforeCreate((user) => {
   return bcrypt
@@ -87,7 +29,7 @@ User.beforeCreate((user) => {
       user.password = hash;
     })
     .catch((err) => {
-      throw new Error();
+      console.log(err);
     });
 });
 
--- site/util/database.js
+++ site/util/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-module.exports = new Sequelize("backend-workshop", "root", "Soulofhell12", {
-    host: "localhost",
-    dialect: "mysql",
+module.exports = new Sequelize("backendWorkshop", "root", "B./CVb*B6@A45Ds", {
+  host: "localhost",
+  dialect: "mysql",
 });
